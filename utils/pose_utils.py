fee64d793f9aea6bb9e6801c62a2dd74b406a478
ParoRahul <r.das699@gmail.com>
2020-06-04 00:58:19 +0530

    Removed tensor cpu error

--- utils/pose_utils.py
+++ utils/pose_utils.py
@@ -13,43 +13,42 @@ from skimage.draw import circle, line_aa, polygon
 np.seterr(divide='ignore', invalid='ignore')
 
 
-LIMB_SEQ = [[1,2], [1,5], [2,3], [3,4], [5,6], [6,7], [1,8], [8,9],
-           [9,10], [1,11], [11,12], [12,13], [1,0], [0,14], [14,16],
-           [0,15], [15,17], [2,16], [5,17]]
+LIMB_SEQ = [[1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7], [1, 8], [8, 9],
+            [9, 10], [1, 11], [11, 12], [12, 13], [1, 0], [0, 14], [14, 16],
+            [0, 15], [15, 17], [2, 16], [5, 17]]
 
 COLORS = [[255, 0, 0], [255, 85, 0], [255, 170, 0], [255, 255, 0], [170, 255, 0], [85, 255, 0], [0, 255, 0],
-          [0, 255, 85], [0, 255, 170], [0, 255, 255], [0, 170, 255], [0, 85, 255], [0, 0, 255], [85, 0, 255],
+          [0, 255, 85], [0, 255, 170], [0, 255, 255], [
+              0, 170, 255], [0, 85, 255], [0, 0, 255], [85, 0, 255],
           [170, 0, 255], [255, 0, 255], [255, 0, 170], [255, 0, 85]]
 
 
 LABELS = ['nose', 'neck', 'Rsho', 'Relb', 'Rwri', 'Lsho', 'Lelb', 'Lwri',
-               'Rhip', 'Rkne', 'Rank', 'Lhip', 'Lkne', 'Lank', 'Leye', 'Reye', 'Lear', 'Rear']
+          'Rhip', 'Rkne', 'Rank', 'Lhip', 'Lkne', 'Lank', 'Leye', 'Reye', 'Lear', 'Rear']
 
 MISSING_VALUE = 0
 
 
-LIMB_SEQ_PAF = [[1,2], [1,5], [2,3], [3,4], [5,6], [6,7], [1,8], [8,9],
-           [9,10], [1,11], [11,12], [12,13], [1,0], [0,14], [14,16],
-           [0,15], [15,17], [2,16], [5,17]]
+LIMB_SEQ_PAF = [[1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7], [1, 8], [8, 9],
+                [9, 10], [1, 11], [11, 12], [12, 13], [1, 0], [0, 14], [14, 16],
+                [0, 15], [15, 17], [2, 16], [5, 17]]
 
 
 LABELS_PAF = ['nose', 'neck', 'Rsho', 'Relb', 'Rwri', 'Lsho', 'Lelb', 'Lwri',
-               'Rhip', 'Rkne', 'Rank', 'Lhip', 'Lkne', 'Lank', 'Leye', 'Reye', 'Lear', 'Rear']
+              'Rhip', 'Rkne', 'Rank', 'Lhip', 'Lkne', 'Lank', 'Leye', 'Reye', 'Lear', 'Rear']
 
-label_colours = [(0,0,0)
-                # 0=Background
-                ,(128,0,0),(255,0,0),(0,85,0),(170,0,51),(255,85,0)
-                # 1=Hat,  2=Hair,    3=Glove, 4=Sunglasses, 5=UpperClothes
-                ,(0,0,85),(0,119,221),(85,85,0),(0,85,85),(85,51,0)
-                # 6=Dress, 7=Coat, 8=Socks, 9=Pants, 10=Jumpsuits
-                ,(52,86,128),(0,128,0),(0,0,255),(51,170,221),(0,255,255)
-                # 11=Scarf, 12=Skirt, 13=Face, 14=LeftArm, 15=RightArm
-                ,(85,255,170),(170,255,85),(255,255,0),(255,170,0)]
-                # 16=LeftLeg, 17=RightLeg, 18=LeftShoe, 19=RightShoe
+label_colours = [(0, 0, 0)                 # 0=Background
+                 # 1=Hat,  2=Hair,    3=Glove, 4=Sunglasses, 5=UpperClothes
+                 # 6=Dress, 7=Coat, 8=Socks, 9=Pants, 10=Jumpsuits
+                 # 11=Scarf, 12=Skirt, 13=Face, 14=LeftArm, 15=RightArm
+                 , (128, 0, 0), (255, 0, 0), (0, 85, 0), (170, 0, 51), (255, 85, 0), (0, 0, 85), (0, 119, 221), (85, 85, 0), (0, 85, 85), (85, 51, 0), (52, 86, 128), (0, 128, 0), (0, 0, 255), (51, 170, 221), (0, 255, 255), (85, 255, 170), (170, 255, 85), (255, 255, 0), (255, 170, 0)]
+# 16=LeftLeg, 17=RightLeg, 18=LeftShoe, 19=RightShoe
 
 # circle embedding
+
+
 def heatmap_embedding(size, pose, radius=6):
-    h, w = size # 128,96
+    h, w = size  # 128,96
     emb_all = np.empty((1, h, w)).astype(np.uint8)
     for item in pose:
         emb = np.zeros((1, h, w))
@@ -62,16 +61,18 @@ def heatmap_embedding(size, pose, radius=6):
                         if distance <= radius:
                             emb[:, x+i, y+j] = 1
         emb_all = np.concatenate((emb_all, emb), axis=0)
-    return emb_all[1:,:,:]
+    return emb_all[1:, :, :]
+
 
 def parsing_embedding(parse_path):
-    parse = Image.open(parse_path) # 256,192
+    parse = Image.open(parse_path)  # 256,192
     parse = np.array(parse)
     parse_emb = []
     for i in range(20):
         parse_emb.append((parse == i).astype(np.float32).tolist())
     parse = np.array(parse_emb).astype(np.float32)
     return parse
+
 
 def pose2loc(pose):
     x = []
@@ -83,25 +84,27 @@ def pose2loc(pose):
             x.append(int(item))
         elif (index + 2) % 3 == 0:
             y.append(int(item))
-    for i,j in zip(x, y):
+    for i, j in zip(x, y):
         if i > 191:
             i = 191
         if j > 255:
             j = 255
-        loc.append([j, i])# attention the indices 
+        loc.append([j, i])  # attention the indices
     return loc
+
 
 def draw_pose_from_cords(pose_joints, img_size, radius=6, draw_joints=True):
 
     colors = np.zeros(shape=img_size + (3, ), dtype=np.uint8)
-    mask = np.zeros(shape=img_size, dtype=bool)   
+    mask = np.zeros(shape=img_size, dtype=bool)
     if draw_joints:
         for f, t in LIMB_SEQ:
             from_missing = pose_joints[f][0] == MISSING_VALUE or pose_joints[f][1] == MISSING_VALUE
             to_missing = pose_joints[t][0] == MISSING_VALUE or pose_joints[t][1] == MISSING_VALUE
             if from_missing or to_missing:
                 continue
-            yy, xx, val = line_aa(pose_joints[f][0], pose_joints[f][1], pose_joints[t][0], pose_joints[t][1])
+            yy, xx, val = line_aa(
+                pose_joints[f][0], pose_joints[f][1], pose_joints[t][0], pose_joints[t][1])
             yy, xx = np.minimum(yy, 255), np.minimum(xx, 191)
             colors[yy, xx] = np.expand_dims(val, 1) * 255
             mask[yy, xx] = True
@@ -115,9 +118,11 @@ def draw_pose_from_cords(pose_joints, img_size, radius=6, draw_joints=True):
 
     return colors, mask
 
+
 def decode_labels(mask, num_images=1, num_classes=20):
     n, h, w, c = mask.shape  # batch_size, 256, 256, 1
-    assert(n >= num_images), 'Batch size %d should be greater or equal than number of images to save %d.' % (n, num_images)
+    assert(n >= num_images), 'Batch size %d should be greater or equal than number of images to save %d.' % (
+        n, num_images)
     outputs = np.zeros((n, h, w, 3), dtype=np.uint8)
     for i in range(n):
         img = Image.new('RGB', (len(mask[i, 0]), len(mask[i])))
@@ -125,16 +130,20 @@ def decode_labels(mask, num_images=1, num_classes=20):
         for j_, j in enumerate(mask[i, :, :, 0]):
             for k_, k in enumerate(j):
                 if k < num_classes:
-                    pixels[k_,j_] = label_colours[k]
+                    pixels[k_, j_] = label_colours[k]
         outputs[i] = np.array(img)
     return outputs
 
+
 def array2img(img_array):
-    imgs = (img_array.permute(0,2,3,1).contiguous().cpu().numpy() * 0.5 + 0.5) * 255
+    imgs = (img_array.permute(
+        0, 2, 3, 1).contiguous().cpu().numpy() * 0.5 + 0.5) * 255
     imgs = imgs.astype(np.uint8)
     return imgs
 
 # all parsing_vis and mask can not use tensor type
+
+
 def save_img(images, path):
     img = []
     assert len(images) > 0
@@ -143,33 +152,36 @@ def save_img(images, path):
             if images[i].shape[3] == 1:
                 images[i] = images[i].repeat(3, axis=3)
             if images[i].shape[3] != 3:
-                images[i] = images[i].transpose((0,2,3,1))
+                images[i] = images[i].transpose((0, 2, 3, 1))
         else:
             if images[i].shape[1] == 1:
-                images[i] = images[i].repeat(1,3,1,1)
+                images[i] = images[i].repeat(1, 3, 1, 1)
             if images[i].shape[1] == 3:
-                images[i] = ((images[i].permute(0,2,3,1).contiguous().cpu().numpy() * 0.5 + 0.5) * 255).astype(np.uint8)
-                 
+                images[i] = ((images[i].permute(0, 2, 3, 1).contiguous(
+                ).cpu().numpy() * 0.5 + 0.5) * 255).astype(np.uint8)
+
     for i in range(len(images[0])):
-        k=[]
+        k = []
         for image in images:
             if isinstance(image[i], np.ndarray):
-                k.append(image)
+                k.append(image[i])
             else:
-                k.append(image.cpu().numpy())     
+                k.append(image[i].cpu().numpy())
         img.append(np.concatenate(k, axis=1))
     img = np.concatenate(img, axis=0)
 
     image = Image.fromarray(img.astype(np.uint8))
     image.save(path)
 
+
 def save_img_grid(images, path):
     assert len(images) > 0
     for i in range(len(images)):
         if isinstance(images[i], np.ndarray):
-            images[i] = torch.from_numpy(images[i].astype(np.float32)).permute(0,3,1,2).contiguous()
+            images[i] = torch.from_numpy(images[i].astype(
+                np.float32)).permute(0, 3, 1, 2).contiguous()
             images[i] = ((images[i] / 255) - 0.5) / 0.5
-                    
+
         else:
             images[i] = images[i].cpu()
     image_vis = []
@@ -178,7 +190,7 @@ def save_img_grid(images, path):
         img = torch.cat([image[i] for image in images], dim=2)
 
         image_vis.append(img.unsqueeze(dim=0))
-    
+
     image_vis = [img.cpu() for img in image_vis]
     image_vis = torch.cat(image_vis, dim=0) * 0.5 + 0.5
 
@@ -208,7 +220,7 @@ def _getSparsePose(peaks, height, width, channel, radius=4, var=4, mode='Solid')
     values = []
     for k in range(len(peaks)):
         p = peaks[k]
-        if 0!=len(p):
+        if 0 != len(p):
             r = p[0][1]
             c = p[0][0]
             ind = _getSparseKeypoint(r, c, k, height, width, radius, var, mode)
@@ -225,40 +237,43 @@ def _sparse2dense(indices, shape):
         r = indices[i][0]
         c = indices[i][1]
         k = indices[i][2]
-        dense[r,c,k] = 1
+        dense[r, c, k] = 1
     return dense
 
 
 # base on the heatmap do erosion to get the mask
 def _getPoseMask(peaks, height, width, radius=4, var=4, mode='Solid'):
-    limbSeq = [[2,3], [2,6], [3,4], [4,5], [6,7], [7,8], [2,9], [9,10], \
-                         [10,11], [2,12], [12,13], [13,14], [2,1], [1,15], [15,17], \
-                         [1,16], [16,18], [2,17], [2,18], [9,12], [12,6], [9,3], [17,18]]
+    limbSeq = [[2, 3], [2, 6], [3, 4], [4, 5], [6, 7], [7, 8], [2, 9], [9, 10],
+               [10, 11], [2, 12], [12, 13], [13, 14], [2, 1], [1, 15], [15, 17],
+               [1, 16], [16, 18], [2, 17], [2, 18], [9, 12], [12, 6], [9, 3], [17, 18]]
     indices = []
     for limb in limbSeq:
-        p0 = peaks[limb[0] -1]
-        p1 = peaks[limb[1] -1]
-        if 0!=len(p0) and 0!=len(p1):
+        p0 = peaks[limb[0] - 1]
+        p1 = peaks[limb[1] - 1]
+        if 0 != len(p0) and 0 != len(p1):
             r0 = p0[1]
             c0 = p0[0]
             r1 = p1[1]
             c1 = p1[0]
-            ind  = _getSparseKeypoint(r0, c0, 0, height, width, radius, var, mode)
+            ind = _getSparseKeypoint(
+                r0, c0, 0, height, width, radius, var, mode)
             indices.extend(ind)
-            ind = _getSparseKeypoint(r1, c1, 0, height, width, radius, var, mode)
+            ind = _getSparseKeypoint(
+                r1, c1, 0, height, width, radius, var, mode)
             indices.extend(ind)
-            
+
             distance = np.sqrt((r0-r1)**2 + (c0-c1)**2)
             sampleN = int(distance/radius)
             if sampleN > 1:
-                for i in range(1,sampleN):
+                for i in range(1, sampleN):
                     r = r0 + (r1-r0)*i/sampleN
                     c = c0 + (c1-c0)*i/sampleN
-                    ind = _getSparseKeypoint(r, c, 0, height, width, radius, var, mode)
+                    ind = _getSparseKeypoint(
+                        r, c, 0, height, width, radius, var, mode)
                     indices.extend(ind)
-                    
+
     shape = [height, width, 1]
-    
+
     dense = np.squeeze(_sparse2dense(indices, shape))
     dense = dilation(dense, square(5))
     dense = erosion(dense, square(5))
@@ -271,8 +286,10 @@ def cords_to_map(cords, img_size, sigma=100):
     for i, point in enumerate(cords):
         if point[0] == MISSING_VALUE or point[1] == MISSING_VALUE:
             continue
-        xx, yy = np.meshgrid(np.arange(img_size[1]), np.arange(img_size[0]))   # 192, 256
-        result[..., i] = np.exp(-((yy - point[0]) ** 2 + (xx - point[1]) ** 2) / (2 * sigma ** 2))
+        xx, yy = np.meshgrid(
+            np.arange(img_size[1]), np.arange(img_size[0]))   # 192, 256
+        result[..., i] = np.exp(-((yy - point[0]) **
+                                  2 + (xx - point[1]) ** 2) / (2 * sigma ** 2))
     return result
 
 
@@ -287,9 +304,9 @@ def map_to_cord(pose_map, pose_dim, threshold=0.1):
     MISSING_VALUE = 0
     all_peaks = [[] for i in range(pose_dim)]
     pose_map = pose_map[..., :pose_dim]
-    
-    y, x, z = np.where(np.logical_and(pose_map == pose_map.max(axis = (0, 1)),
-                                     pose_map > threshold))
+
+    y, x, z = np.where(np.logical_and(pose_map == pose_map.max(axis=(0, 1)),
+                                      pose_map > threshold))
     for x_i, y_i, z_i in zip(x, y, z):
         all_peaks[z_i].append([x_i, y_i])
 
@@ -309,7 +326,7 @@ def map_to_cord(pose_map, pose_dim, threshold=0.1):
 
 def compute_pose_map(self, pair, direction):
     assert direction in ['to', 'from']
-    
+
     pose_map = np.empty(list(self._image_size) + [self.pose_dim])
     row = self._annotations_file.loc[pair[direction]]
     # file_name = self._tmp_pose + pair[direction] + '.npy'
@@ -318,32 +335,32 @@ def compute_pose_map(self, pair, direction):
     # else:
 
     # pose cordinate
-    kp_array = load_pose_cords_from_strings(row['keypoints_y'], row['keypoints_x'])
-
+    kp_array = load_pose_cords_from_strings(
+        row['keypoints_y'], row['keypoints_x'])
 
     pose_map = cords_to_map(kp_array, self._image_size)
     # np.save(file_name, pose)
     # channels must be first
-    return np.transpose(pose_map, [2,0,1])
-
+    return np.transpose(pose_map, [2, 0, 1])
 
 
 def compute_interpol_map(inp_map, tg_map):
-    
+
     # map to cord expects channels in last dim (old keras code)
     num_stacks = 5
     pose_dim = 18
-    _image_size = (256,192)
+    _image_size = (256, 192)
     # source_cord = pose_utils.map_to_cord(source_map, 16, 0.1)
     inp_pos = map_to_cord(inp_map, pose_dim)
     tg_pos = map_to_cord(tg_map, pose_dim)
     pose_maps = []
     # compute interpol poses equal to num_stacks, with final pose being equal to the target psoe
-    for i in range(1,num_stacks+1):
-        interpol_pose = compute_interpol_pose(inp_pos,tg_pos,i, num_stacks, pose_dim)
+    for i in range(1, num_stacks+1):
+        interpol_pose = compute_interpol_pose(
+            inp_pos, tg_pos, i, num_stacks, pose_dim)
         # print(interpol_pose)
         interpol_pose_map = cords_to_map(interpol_pose, _image_size)
-        pose_maps.append(np.transpose(interpol_pose_map, [2,0,1]))
+        pose_maps.append(np.transpose(interpol_pose_map, [2, 0, 1]))
     return pose_maps
 
 
@@ -353,10 +370,10 @@ def compute_interpol_cord_warp(inp_map, interpol_pose):
     interpol_warps, interpol_masks = [], []
     _warp_skip = 'mask'
     pose_dim = 18
-    _image_size = (256,192)
+    _image_size = (256, 192)
     # possibly causing nan error
     kp_array1 = map_to_cord(inp_map, pose_dim)
-    
+
     for pose in interpol_pose:
         if _warp_skip == 'full':
             warp = [np.empty([1, 8]), 1]
@@ -366,30 +383,33 @@ def compute_interpol_cord_warp(inp_map, interpol_pose):
         kp_array2 = map_to_cord(pose, pose_dim)
 
         if _warp_skip == 'mask':
-            warp[0] = pose_transform.affine_transforms(kp_array1, kp_array2, pose_dim)
-            warp[1] = pose_transform.pose_masks(kp_array2, _image_size, pose_dim)
+            warp[0] = pose_transform.affine_transforms(
+                kp_array1, kp_array2, pose_dim)
+            warp[1] = pose_transform.pose_masks(
+                kp_array2, _image_size, pose_dim)
         else:
-            warp[0] = pose_transform.estimate_uniform_transform(kp_array1, kp_array2, pose_dim)
+            warp[0] = pose_transform.estimate_uniform_transform(
+                kp_array1, kp_array2, pose_dim)
         interpol_warps.append(warp[0])
         interpol_masks.append(warp[1])
 
         kp_array1 = kp_array2
-        
+
     return np.array(interpol_warps), np.array(interpol_masks)
 
 
-def compute_interpol_pose(inp_pos,tg_pos,index,num_stacks,pose_dim):
+def compute_interpol_pose(inp_pos, tg_pos, index, num_stacks, pose_dim):
     assert index <= num_stacks
-    if(pose_dim==16):
+    if(pose_dim == 16):
         interpol_pose = inp_pos + (tg_pos-inp_pos)*index/num_stacks
     # bad logic to circumvent missing annot . . synthesize and vanish missing annot after half sequence is completed
-    elif(pose_dim==18):                                                                                                                   
-        interpol_pose = np.zeros([pose_dim,2], dtype='float32')
+    elif(pose_dim == 18):
+        interpol_pose = np.zeros([pose_dim, 2], dtype='float32')
         for i in range(pose_dim):
             # inp pose has missing annot and tg pose has it
-            if ((inp_pos[i,0] == MISSING_VALUE or inp_pos[i,1]== MISSING_VALUE) and
-                    (tg_pos[i,0] != MISSING_VALUE and tg_pos[i,1]!= MISSING_VALUE)):
-                if(index<=num_stacks//2):
+            if ((inp_pos[i, 0] == MISSING_VALUE or inp_pos[i, 1] == MISSING_VALUE) and
+                    (tg_pos[i, 0] != MISSING_VALUE and tg_pos[i, 1] != MISSING_VALUE)):
+                if(index <= num_stacks//2):
                     interpol_pose[i] = MISSING_VALUE
                 else:
                     interpol_pose[i] = tg_pos[i]
@@ -406,51 +426,45 @@ def compute_interpol_pose(inp_pos,tg_pos,index,num_stacks,pose_dim):
                 interpol_pose[i] = MISSING_VALUE
             # normal interpol when annot are present in both cases
             else:
-                interpol_pose[i] = inp_pos[i] + (tg_pos[i]-inp_pos[i])*index/num_stacks
+                interpol_pose[i] = inp_pos[i] + \
+                    (tg_pos[i]-inp_pos[i])*index/num_stacks
     return interpol_pose
 
 
-def compute_cord_warp(source_pose_loc, target_pose_loc, _warp_skip = 'mask', _image_size=(256,192), pose_dim=16):
-    
-    _image_size = (256,192)
+def compute_cord_warp(source_pose_loc, target_pose_loc, _warp_skip='mask', _image_size=(256, 192), pose_dim=16):
+
+    _image_size = (256, 192)
     if _warp_skip == 'full':
         # warp = [np.empty([1, pose_dim-10]), 1]
-        warp = [np.empty([1,8]), 1]
+        warp = [np.empty([1, 8]), 1]
     else:
         # warp = [np.empty([10, pose_dim-10]),
         #         np.empty([10] + list(_image_size))]
         warp = [np.empty([10, 8]),
-            np.empty([10] + list(_image_size))]
+                np.empty([10] + list(_image_size))]
 
     kp_array1 = source_pose_loc
     kp_array2 = target_pose_loc
 
     if _warp_skip == 'mask':
-        warp[0] = pose_transform.affine_transforms(kp_array1, kp_array2, pose_dim)
+        warp[0] = pose_transform.affine_transforms(
+            kp_array1, kp_array2, pose_dim)
         warp[1] = pose_transform.pose_masks(kp_array2, _image_size, pose_dim)
     else:
-        warp[0] = pose_transform.estimate_uniform_transform(kp_array1, kp_array2, pose_dim)
+        warp[0] = pose_transform.estimate_uniform_transform(
+            kp_array1, kp_array2, pose_dim)
     return warp
+
 
 def downsample_pose_array(input_array):
     kp_array = input_array.copy()
     for idx in range(18):
-        if kp_array[idx,0] != -1 and kp_array[idx,1] != -1:
-            kp_array[idx,0] = int(kp_array[idx,0] * 0.5)
-            kp_array[idx,1] = int(kp_array[idx,1] * 0.5)
+        if kp_array[idx, 0] != -1 and kp_array[idx, 1] != -1:
+            kp_array[idx, 0] = int(kp_array[idx, 0] * 0.5)
+            kp_array[idx, 1] = int(kp_array[idx, 1] * 0.5)
 
     return kp_array
 
+
 if __name__ == '__main__':
     pass
-    
-
-
-
-
-
-
-
-
-    
-
